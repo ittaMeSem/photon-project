43e5634d7b5189f73e339e27b7ead8101610a05f
Sami Ciocan <77675027+ittaMeSem@users.noreply.github.com>
2022-09-12 13:06:12 +0300

    project init, js fetching

---
+++ app.js
@@ -0,0 +1,28 @@
+const auth = "563492ad6f9170000100000137d1798e3e8a4595ac8a0b1aacdb0100";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitButton = document.querySelector(".submit-btn");
+let searchValue;
+
+async function curatedPhotos() {
+  const dataFetch = await fetch(
+    "https://api.pexels.com/v1/curated?per_page=15&page=1",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+        <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+curatedPhotos();
