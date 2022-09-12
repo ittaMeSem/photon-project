f6b5f62e864d9fcde424b0ee47f56fa40d99b289
Sami Ciocan <77675027+ittaMeSem@users.noreply.github.com>
2022-09-12 13:46:28 +0300

    main styling, clear search input, load more init

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
 const form = document.querySelector(".search-form");
 let searchValue;
+const more = document.querySelector(".more");
 
 //Event listeners
 searchInput.addEventListener("input", updateInput);
@@ -10,7 +11,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   searchPhotos(searchValue);
 });
-
+more.addEventListener("click", loadMore);
 //Functions & api generation
 
 function updateInput(e) {
@@ -33,8 +34,13 @@ function generatePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
-    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-        <p>${photo.photographer}</p>`;
+    galleryImg.innerHTML = `
+    <div class="gallery-info">
+     <p>${photo.photographer}</p>
+        <a href=${photo.src.original}>Download</a>
+    </div>
+    <img src=${photo.src.large}></img>
+    `;
     gallery.appendChild(galleryImg);
   });
 }
@@ -47,10 +53,20 @@ async function curatedPhotos() {
 }
 
 async function searchPhotos(query) {
+  clear();
   const data = await fetchAPI(
     `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
   );
   generatePictures(data);
+}
+
+function clear() {
+  gallery.innerHTML = "";
+  searchInput.value = "";
+}
+
+async function loadMore() {
+    
 }
 
 curatedPhotos();
