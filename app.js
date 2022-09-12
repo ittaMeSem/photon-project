ac02c5d56144ab5a790590010120affe693b48dd
Sami Ciocan <77675027+ittaMeSem@users.noreply.github.com>
2022-09-12 14:20:14 +0300

    photon project fully functional

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const searchInput = document.querySelector(".search-input");
 const form = document.querySelector(".search-form");
 let searchValue;
 const more = document.querySelector(".more");
+let page = 1;
+let fetchLink;
+let currentSearch;
 
 //Event listeners
 searchInput.addEventListener("input", updateInput);
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  currentSearch = searchValue;
   searchPhotos(searchValue);
 });
 more.addEventListener("click", loadMore);
-//Functions & api generation
 
+//Functions & api generation
 function updateInput(e) {
   searchValue = e.target.value;
 }
@@ -46,17 +50,15 @@ function generatePictures(data) {
 }
 
 async function curatedPhotos() {
-  const data = await fetchAPI(
-    "https://api.pexels.com/v1/curated?per_page=15&page=1"
-  );
+  fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+  const data = await fetchAPI(fetchLink);
   generatePictures(data);
 }
 
 async function searchPhotos(query) {
   clear();
-  const data = await fetchAPI(
-    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
-  );
+  fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`;
+  const data = await fetchAPI(fetchLink);
   generatePictures(data);
 }
 
@@ -66,7 +68,14 @@ function clear() {
 }
 
 async function loadMore() {
-    
+  page++;
+  if (currentSearch) {
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page=${page}`;
+  } else {
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+  }
+  const data = await fetchAPI(fetchLink);
+  generatePictures(data);
 }
 
 curatedPhotos();
